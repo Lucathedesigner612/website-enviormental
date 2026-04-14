bab0e70c126885ae56fa9324446fa89a9e84e1fc
Lucathedesigner612 <lucagalea612@gmail.com>
2026-04-14 19:10:09 +0200

    Update page_home.py

--- page_home.py
+++ page_home.py
@@ -16,10 +16,17 @@ with col1:
     st.write("Constant traffic congestion in narrow village cores leads to high decibel levels and resident burnout.")
 
 with col2:
-    # This acts as your 'Visual Map' placeholder
-    st.info("🗺️ Interactive Map Analysis")
-    st.image("https://www.google.com/maps/@35.8817017,14.4852276,3a,75y,304.02h,87.72t/data=!3m7!1e1!3m5!1sZZndRNNn8ZPZRn_sed8fAg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D2.2797142157755417%26panoid%3DZZndRNNn8ZPZRn_sed8fAg%26yaw%3D304.02009826192295!7i13312!8i6656!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDQwOC4wIKXMDSoASAFQAw%3D%3D", 
-             caption="Current Traffic Bottlenecks in the Village Core")
+
+# The link you want to open
+url = "https://www.google.com/maps/@35.8817017,14.4852276,3a,75y,304.02h,87.72t/data=!3m7!1e1!3m5!1sZZndRNNn8ZPZRn_sed8fAg!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D2.2797142157755417%26panoid%3DZZndRNNn8ZPZRn_sed8fAg%26yaw%3D304.02009826192295!7i13312!8i6656!5m1!1e1?authuser=0&entry=ttu&g_ep=EgoyMDI2MDQwOC4wIKXMDSoASAFQAw%3D%3D"
+
+# The image you want to show
+img_url = "https://path-to-your-image.jpg"
+
+st.markdown(
+    f'<a href="{url}" target="_blank"><img src="{img_url}" width="100%"></a>',
+    unsafe_allow_html=True
+)
 
 st.divider()
 
